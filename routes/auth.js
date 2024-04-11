21dac53085ac1015b939bd940fb28f05a43383cc
Tara Denniston <taradenniston@gmail.com>
2024-04-10 22:25:27 -0400

    add logic to /login route to check credentials

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/user');
 const router = new express.Router();
+const ExpressError = require('../expressError');
 
 
 /** POST /login - login: {username, password} => {token}
@@ -8,6 +9,21 @@ const router = new express.Router();
  * Make sure to update their last-login!
  *
  **/
+router.post('/login', async (req, res, next) => {
+  try{
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new ExpressError('Username and password required', 400);
+    }
+    if (await User.authenticate(username, password)) {
+      return res.json('Login successful');
+    } else {
+      throw new ExpressError('Pssword is incorrect', 400);
+    }
+  } catch (e) {
+    return next(e);
+  }
+});
 
 
 /** POST /register - register user: registers, logs in, and returns token.
@@ -19,17 +35,24 @@ const router = new express.Router();
 router.post('/register', async (req, res, next) => {
   try {
     console.log('Inside /register route')
+    // Register user
     const user = await User.register(req.body);
     console.log('/register - user:')
     console.log(user);
+    // Update last login
     const timeStamp = await User.updateLoginTimestamp(user.username);
     console.log('/register - timeStamp:')
     console.log(timeStamp);
+    // Create token (to be implemented)
+    // Return token (to be implemented)
     return res.json('User Registered');
   } catch (e) {
+    if (e.code === '23505') {
+      return next(new ExpressError('Username already exists', 400));
+    }
     return next(e);
   }
-})
+});
 
 
 module.exports = router;
