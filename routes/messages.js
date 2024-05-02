d51cb91c760c358fee2f7a66808cf5f620d3d9c7
Tara Denniston <taradenniston@gmail.com>
2024-05-01 21:21:18 -0400

    add post route for /

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,9 @@
+const express = require('express');
+const router = new express.Router();
+const Message = require('../models/message');
+const ExpressError = require('../expressError');
+const { ensureLoggedIn } = require('../middleware/auth')
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -10,6 +16,18 @@
  * Make sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get('/:id', ensureLoggedIn, async (req, res, next) => {
+  const id = req.params.id;
+  const message = await Message.get(id);
+
+  // Throw error if the logged-in user is not either the sender or recipient
+  if(!(message.from_user === req.user.username || 
+      message.to_user === req.user.username)) {
+    throw new ExpressError(`User is unauthorized to access message ${id})`, 401);
+  }
+
+  return res.json({ message: message });
+});
 
 
 /** POST / - post message.
@@ -18,6 +36,12 @@
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post('/', ensureLoggedIn, async (req, res, next) => {
+  const from_username = req.user.username;
+  const { to_username, body } = req.body;
+  const message = await Message.create({from_username, to_username, body});
+  return res.json({ message: message });
+});
 
 
 /** POST/:id/read - mark message as read:
@@ -27,4 +51,7 @@
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
+
+
+module.exports = router;
 
